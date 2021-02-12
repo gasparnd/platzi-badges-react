aff61e41a41b5e8a9817a5d8e5386518c750b940
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-02-12 11:59:32 -0300

    test

---
+++ build/precache-manifest.3e21dc78711252ac617d3d4690801886.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bcc4e88c70a505f5c2f",
+    "url": "/gasparnd/react-course/static/css/main.590d0000.chunk.css"
+  },
+  {
+    "revision": "9bcc4e88c70a505f5c2f",
+    "url": "/gasparnd/react-course/static/js/main.9bcc4e88.chunk.js"
+  },
+  {
+    "revision": "e6ac4da6a19acd6fbff2",
+    "url": "/gasparnd/react-course/static/js/runtime~main.e6ac4da6.js"
+  },
+  {
+    "revision": "dbc1dae0896fa89d47c0",
+    "url": "/gasparnd/react-course/static/css/2.9976ea66.chunk.css"
+  },
+  {
+    "revision": "dbc1dae0896fa89d47c0",
+    "url": "/gasparnd/react-course/static/js/2.dbc1dae0.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/gasparnd/react-course/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/gasparnd/react-course/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/gasparnd/react-course/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/gasparnd/react-course/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "9ebd7b36f8c87f09519d5a96a9629fb6",
+    "url": "/gasparnd/react-course/index.html"
+  }
+];
